7273d801877174824ba4b13594deaa9ca6e0099f
Baxrom <bahrom9229nosirov@gmail.com>
2026-01-12 14:10:04 +0500

    adding total count to cart page

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,11 +1,24 @@
 <template>
   <main class="cart">
     <PathShower :paths />
+    <div class="container">
+      <h1 class="title">Моя корзина</h1>
+      <span>{{ !cartProducts ? 0 : cartProducts }} товара</span>
+    </div>
   </main>
 </template>
 
 <script setup>
   import PathShower from '@/components/PathShower.vue';
+  import { computed } from 'vue';
+  import {useStore} from 'vuex'
+
+  const store = useStore()
+
+  const cartProducts = computed(() => {
+    store.getters["cart/cartCount"]
+  })
+
 
   const paths = [
     {
@@ -21,8 +34,21 @@
 
 </script>
 
-<style>
+<style scoped>
 .cart{
   margin-top: 30px;
 }
+
+.title{
+  font-weight: 500;
+  font-style: Medium;
+  font-size: 40px;
+  leading-trim: NONE;
+  line-height: 100%;
+  letter-spacing: 0%;
+  text-transform: uppercase;
+  margin-bottom: 15px;
+  margin-top: 50px;
+}
+
 </style>
